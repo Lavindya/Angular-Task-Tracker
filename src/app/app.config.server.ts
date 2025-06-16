75fc29d7bd7e11c9efd7f90424407affb1b4e249
Lavindya Manuji <lavindya.20232465@iit.ac.lk>
2025-06-16 11:31:32 +0530

    Remove src folder

--- src/app/app.config.server.ts
+++
@@ -1,9 +0,0 @@
-import { mergeApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { appConfig } from './app.config';
-
-export default mergeApplicationConfig(appConfig, {
-  providers: [
-    provideServerRendering()
-  ]
-});
